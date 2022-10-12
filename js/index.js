5b4b0648eab4b9f9d2cff8ac07d0b81513fbd47d
VaNomad <sjur.hassel@gmail.com>
2022-10-12 21:26:54 +0100

    slider resize adjustments

--- js/index.js
+++ js/index.js
@@ -1,31 +1,31 @@
 // ——————————————————————————————————— Navigation ————————————
 // Cursor
-let innerCursor = document.querySelector(".inner-cursor");
-let outerCursor = document.querySelector(".outer-cursor");
+// let innerCursor = document.querySelector(".inner-cursor");
+// let outerCursor = document.querySelector(".outer-cursor");
 
-document.addEventListener('mousemove', moveCursor);
+// document.addEventListener('mousemove', moveCursor);
 
-function moveCursor(e) {
-  let x = e.clientX;
-  let y = e.clientY;
+// function moveCursor(e) {
+//   let x = e.clientX;
+//   let y = e.clientY;
 
-  innerCursor.style.left = `${x}px`;
-  innerCursor.style.top = `${y}px`;
-  outerCursor.style.left = `${x}px`;
-  outerCursor.style.top = `${y}px`;
-};
+//   innerCursor.style.left = `${x}px`;
+//   innerCursor.style.top = `${y}px`;
+//   outerCursor.style.left = `${x}px`;
+//   outerCursor.style.top = `${y}px`;
+// };
 
-let links = Array.from(document.querySelectorAll("a, button, span, i, .phone, .myname, .email"));
-console.log(links);
+// let links = Array.from(document.querySelectorAll("a, button, span, i, .myname, .email"));
+// console.log(links);
 
-links.forEach((link) => {
-  link.addEventListener("mouseover", () => {
-    innerCursor.classList.add("grow");
-  });
-  link.addEventListener("mouseleave", () => {
-    innerCursor.classList.remove("grow");
-  });
-});
+// links.forEach((link) => {
+//   link.addEventListener("mouseover", () => {
+//     innerCursor.classList.add("grow");
+//   });
+//   link.addEventListener("mouseleave", () => {
+//     innerCursor.classList.remove("grow");
+//   });
+// });
 
 
 // ——————————————————————————————————— Hero section —————————— 
---
+++ js/utls/nav.js
@@ -0,0 +1,20 @@
+/* —————————  Global  ————————————————————————————————————————————— */
+document.querySelectorAll(".nav-link").forEach(link => {
+  if (link.href === window.location.href) {
+    link.setAttribute('aria-current', 'page')
+  }
+  
+})
+
+/* —————————  mobile menu  ———————————————————————————————————————— */
+const mobile = document.querySelector(".mobile");
+const navMenu = document.querySelector(".nav-menu");
+const logo = document.querySelector(".logo-mobile");
+
+
+const mobileMenu = () => {
+  mobile.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+mobile.addEventListener("click", mobileMenu);
