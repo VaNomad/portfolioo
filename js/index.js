98c1e4eaad4f12eba055b9822edc36db5ab517eb
VaNomad <sjur.hassel@gmail.com>
2022-10-12 17:38:59 +0100

    new slider

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ window.onscroll = () => {
 
 // ——————————————————————————————————— 2nd Slider —————————— 
 
-const slidesContainer = document.querySelector("#slides-container");
+const slidesContainer = document.querySelector(".slides-container");
 const slide = document.querySelector(".slide");
 const prevButton = document.querySelector("#slide-arrow-prev");
 const nextButton = document.querySelector("#slide-arrow-next");
