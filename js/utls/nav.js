d4ba008f6390025fd39f7493053544ad2ef5bdeb
VaNomad <sjur.hassel@gmail.com>
2022-10-13 22:52:08 +0100

    startet on projects page and cards

--- js/utls/nav.js
+++ js/utls/nav.js
@@ -8,14 +8,17 @@ document.querySelectorAll(".nav-link").forEach(link => {
 
 /* —————————  mobile menu  ———————————————————————————————————————— */
 const mobile = document.querySelector(".mobile");
+const mobile2 = document.querySelector(".mobile2");
 const navMenu = document.querySelector(".nav-menu");
 const logo = document.querySelector(".logo-mobile");
 
 
 const mobileMenu = () => {
   mobile.classList.toggle("active");
+  mobile2.classList.toggle("active");
   navMenu.classList.toggle("active");
   logo.classList.toggle("active");
 }
 
 mobile.addEventListener("click", mobileMenu);
+mobile2.addEventListener("click", mobileMenu);
